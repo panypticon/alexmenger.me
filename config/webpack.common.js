d8260c3cc2dcfa07da4fec79962f3e18ee35cee3
Alex <alex.menger@digitalcareerinstitute.org>
2021-09-13 21:39:20 +0200

    change deployment, webpack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -14,7 +14,8 @@ module.exports = {
     output: {
         path: paths.build,
         filename: '[name].bundle.js',
-        publicPath: '/'
+        publicPath: '/',
+        assetModuleFilename: 'images/[hash][ext][query]'
     },
 
     // Customize the webpack build process
