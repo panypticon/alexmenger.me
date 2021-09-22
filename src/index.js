d75e37d83ce607d0a3c0576cca72e80fbefa2991
Alex <alex.menger@digitalcareerinstitute.org>
2021-09-22 10:30:58 +0200

    Burger menu button logic

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 // Import styles
 import '@/sass/index.scss';
+
+document.addEventListener('DOMContentLoaded', () => {
+    // DOM Selectors
+    const menuIcon = document.querySelector('.nav__icon');
+
+    // Event Listeners
+    menuIcon.addEventListener('click', () => menuIcon.classList.toggle('nav__icon--close'));
+});
