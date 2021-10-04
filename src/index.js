c324debeafbf3b942d3baab2634bfca819030da1
Alex <alex.menger@digitalcareerinstitute.org>
2021-10-04 17:41:04 +0200

    whatido / code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+
 import SegmentedControl from './scripts/segmented-control';
 import Card from './scripts/card';
 
@@ -7,12 +10,14 @@ document.addEventListener('DOMContentLoaded', () => {
     // DOM Selectors
     const menuIcon = document.querySelector('.nav__icon');
     const whatIDoSegmented = document.querySelector('.whatido .segmented-control');
-    const whatIDoUX = document.querySelector('.whatido .cardview--ux');
-    const whatIDoCode = document.querySelector('.whatido .view-code');
+    const whatIDoUX = document.querySelector('.whatido .cardview');
+    const whatIDoCode = document.querySelector('.whatido .codeview');
     const whatIDoUXCards = whatIDoUX.querySelectorAll('.card');
 
     // Event Listeners and Initialization
     menuIcon.addEventListener('click', () => menuIcon.classList.toggle('nav__icon--close'));
     whatIDoSegmented && whatIDoUX && whatIDoCode && new SegmentedControl(whatIDoSegmented, whatIDoUX, whatIDoCode);
     whatIDoUXCards.forEach(card => new Card(card));
+    hljs.registerLanguage('javascript', javascript);
+    hljs.highlightAll();
 });
