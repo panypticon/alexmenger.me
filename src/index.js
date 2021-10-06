155a293d404e27e46cb55cd0973835b7a6dd3676
Alex <alex.menger@digitalcareerinstitute.org>
2021-10-06 12:10:17 +0200

    Prevent scrolling behind nav on touch devices

--- src/index.js
+++ src/index.js
@@ -15,9 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const whatIDoCode = document.querySelector('.whatido .codeview');
     const whatIDoUXCards = whatIDoUX.querySelectorAll('.card');
     const whyICareItems = document.querySelectorAll('.whyicare__item');
+    const nav = document.querySelector('.nav__main');
+    const navIcon = nav.querySelector('.nav__icon');
 
     // Event Listeners and Initialization
     menuIcon.addEventListener('click', () => menuIcon.classList.toggle('nav__icon--close'));
+    // Prevent scrolling when mobile nav modal is open
+    nav.addEventListener('touchmove', evt => navIcon.classList.contains('nav__icon--close') && evt.preventDefault());
     whatIDoSegmented && whatIDoUX && whatIDoCode && new SegmentedControl(whatIDoSegmented, whatIDoUX, whatIDoCode);
     whatIDoUXCards.forEach(card => new WhatIDoCard(card));
     whyICareItems.forEach(item => new WhyICareItem(item));
