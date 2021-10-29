48ae417520d4da407fce137dec1e5a063d21821c
Alex <alex.menger@digitalcareerinstitute.org>
2021-10-29 20:49:07 +0200

    Sticky nav

--- src/scripts/nav.js
+++ src/scripts/nav.js
@@ -5,7 +5,7 @@ export const handleNavClick = (evt, navIcon) => {
         navIcon.classList.remove('nav__icon--close');
         const destination = document.getElementById(target);
         window.scrollTo({
-            top: destination.offsetTop,
+            top: destination.offsetTop - 60,
             left: 0,
             behavior: 'smooth'
         });
@@ -17,23 +17,13 @@ export const handleStickyNav = nav => {
     else nav.classList.remove('sticky');
 };
 
-export class Section {
-    constructor(elem, navItems) {
-        this.section = elem;
-        this.navItems = navItems;
-        this.handleIntersectionObserver = this.handleIntersectionObserver.bind(this);
-        this.observer = new IntersectionObserver(this.handleIntersectionObserver, {
-            rootMargin: '50px 0px 0px 0px',
-            threshold: 0.33
-        });
-        this.observer.observe(this.section);
-    }
-
-    handleIntersectionObserver(entry) {
-        entry[0].isIntersecting &&
-            this.navItems.forEach(item => {
-                if (item.children[0].dataset.target === this.section.id) item.classList.add('nav__list--active');
+export const handleCurrentNavItem = (sections, navItems) => {
+    sections.forEach(section => {
+        const { top } = section.getBoundingClientRect();
+        if (top >= -150 && top <= 150)
+            navItems.forEach(item => {
+                if (item.children[0].dataset.target === section.id) item.classList.add('nav__list--active');
                 else item.classList.remove('nav__list--active');
             });
-    }
-}
+    });
+};
