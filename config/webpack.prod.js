5b73a8b811abc9c3313c2331b99e2bec9d7ac040
Alex <alex.menger@digitalcareerinstitute.org>
2021-11-04 17:10:45 +0100

    Deployment and Google

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
     devtool: false,
     output: {
         path: paths.build,
-        publicPath: '/alexmenger.me/',
+        publicPath: '/',
         filename: 'js/[name].[contenthash].bundle.js'
     },
     module: {
