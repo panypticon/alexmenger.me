4436762adc9fd409d0c07d956e74fe3614db59c7
Alex <alex.menger@digitalcareerinstitute.org>
2021-11-04 11:54:30 +0100

    Switch deployment to GH Pages

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
     devtool: false,
     output: {
         path: paths.build,
-        publicPath: '/',
+        publicPath: '/alexmenger.me/',
         filename: 'js/[name].[contenthash].bundle.js'
     },
     module: {
